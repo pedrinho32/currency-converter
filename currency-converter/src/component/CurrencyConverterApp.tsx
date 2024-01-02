4ef10af8aec72913aaa6a96f118e647898f95437
Josef Voborník <josef.vobornik01@gmail.com>
2024-01-02 12:01:45 +0100

    parse response to currencies

--- currency-converter/src/component/CurrencyConverterApp.tsx
+++ currency-converter/src/component/CurrencyConverterApp.tsx
@@ -1,8 +1,32 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../css/App.css';
 import {useCnbApi} from "../hook/CnbApiHook";
+import {CurrencyInfo} from "../model/CnbRatesResponse";
 
 const CurrencyConverterApp: React.FC = () => {
+    const {fetchRates} = useCnbApi();
+
+    const [currencies, setCurrencies] = useState<CurrencyInfo[]>();
+
+    useEffect(() => {
+        fetchRates().then(resp => {
+            const respLines: string[] = resp.data.split("\n");
+            const respCurrencies = respLines
+                .slice(2, respLines.length-1)
+                .map(line => {
+                    const splitLine = line.split("|");
+                    return {
+                        country: splitLine[0],
+                        currency: splitLine[1],
+                        amount: Number(splitLine[2]),
+                        code: splitLine[3],
+                        rate: Number(splitLine[4]),
+                    };
+                });
+
+            setCurrencies(respCurrencies);
+        });
+    }, []);
 
     return (
         <div className="App">
