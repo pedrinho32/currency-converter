310ac6616841c3e79bfe6a25f59bdb9954477e1a
Josef Voborník <josef.vobornik01@gmail.com>
2024-01-04 12:13:03 +0100

    setting server url

--- client/src/hook/CnbApiHook.tsx
+++ client/src/hook/CnbApiHook.tsx
@@ -3,7 +3,7 @@ import axios, {AxiosResponse} from "axios";
 export const useCnbApi = () => {
 
     const fetchRates = (): Promise<AxiosResponse<string>> => {
-        return axios.get("http://localhost:3001/api/exchange-rates");
+        return axios.get("https://currency-converter-server-2.vercel.app/api/exchange-rates");
     };
 
     return {
